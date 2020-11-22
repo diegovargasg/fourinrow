69f4d4e5a7c8a15588f731237d164e703685b497
diegovargasg <diegodevelopero@gmail.com>
2020-11-22 17:16:05 +0100

    Added create game form builder

--- client/src/app/create/create.component.ts
+++ client/src/app/create/create.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectionService } from '../connection/connection.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  createForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private connectionService: ConnectionService,
+    private formBuilder: FormBuilder
+  ) {
+    this.createForm = formBuilder.group({
+      name: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  connect() {
+    this.connectionService.connect();
+  }
+
+  onSubmit() {
+    const formValues: {} = Object.assign({}, this.createForm.value);
+    this.connect();
+    console.log(formValues);
+  }
 }
