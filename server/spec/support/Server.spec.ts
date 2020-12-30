8d1d9503dcb938b9ea9fdda8fceb7ac5e83c613d
Diego Vargas <diegodevelopero@gmail.com>
2020-12-30 06:53:36 +0100

    Update Server.spec.ts
    possible implementation for Jasmine Server testing

--- server/spec/support/Server.spec.ts
+++ server/spec/support/Server.spec.ts
@@ -1,8 +1,28 @@
 import jasmine from "jasmine";
 
-describe("something", () => {
-  const i = true;
-  it("should work", () => {
-    expect(i).toBe(true);
-  });
+beforeEach(async function() {
+  //await someLongSetupFunction();
+});
+
+it('creates a game', async function() {
+  const game = await createGame();
+  expect(game).toEqual(GameObject);
+});
+
+it('creates a player', async function() {
+  const player = await createGame();
+  expect(player).toEqual(PlayerObject);
+});
+
+it('joins a game', async function() {
+  await joinGame(gameId, playerName);
+  gameObject = await getGame(gameId);
+  await expect(gameObject).toContains(PlayerObject);
+});
+
+it('set a player ready', async function() {
+  await playerReady(gameId, playerId);
+  gameObject = await getGame(gameId);
+  playerObject = await getPlayerById(playerId);
+  await expect(playerObject.isReady).toBe(true);
 });
