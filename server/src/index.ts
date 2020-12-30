2cf4d27bf8ae4cfef674f76e96a72eb7eefd5614
diegovargasg <diegodevelopero@gmail.com>
2020-12-30 02:40:08 +0100

    modified start point for using interfaces and models

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,56 +1,19 @@
-import express from "express";
-import http from "http";
+import "reflect-metadata";
+import { Server } from "../models/Server";
+import { ServerWebsockets } from "../services/Server.websockets.service";
+import { DAO } from "../models/Dao";
+import { DAORedis } from "../services/Dao.redis.service";
+import { container } from "tsyringe";
 
-require("dotenv").config();
-
-const redis = require("redis");
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  retry_strategy: () => 1000,
+container.register("DAOService", {
+  useClass: DAO,
+});
+container.register("ServerService", {
+  useClass: Server,
 });
 
-redisClient.on("errors", function (err: any) {
-  console.log("Error + err");
-});
+const dao = container.resolve(DAORedis);
+dao.init();
 
-const app = express();
-const server = http.createServer(app);
-const port = process.env.PORT || "5000";
-const options = {
-  cors: {
-    origin: "http://localhost:4200",
-    methods: ["GET", "POST"],
-  },
-};
-const io = require("socket.io")(server, options);
-
-//Run when client connects
-io.on("connection", (socket: any) => {
-  console.log("User connected: ", socket.id);
-
-  socket.on("create_Game", async (data: any) => {
-    const { game, player } = data;
-    console.log("Game Created", game);
-    redisClient.set("gameId", "{'id':1234s5}");
-  });
-
-  socket.on("joinGame", async (data: any) => {
-    console.log("user joined", data);
-  });
-
-  socket.on("disconnect", async () => {
-    console.log("User disconnected", socket.id);
-  });
-});
-
-//Runs server
-server.listen(port, async () => {
-  try {
-    console.log(`Servers running in   ${port}`);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-module.exports = server;
+const server = container.resolve(ServerWebsockets);
+server.init(dao);
