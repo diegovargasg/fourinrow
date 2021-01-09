67ae5d74135f47cc06327a823fd0476e0fd41e70
diegovargasg <diegodevelopero@gmail.com>
2021-01-09 22:53:49 +0100

    added additional angular material components

--- client/src/app/shared/material/material.module.ts
+++ client/src/app/shared/material/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [MatButtonModule, MatInputModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatInputModule, MatFormFieldModule],
 })
 export class MaterialModule {}
