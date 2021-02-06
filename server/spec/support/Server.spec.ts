08a16876b88dee2c973c31e294a5a03af9f6193f
diegovargasg <diegodevelopero@gmail.com>
2021-02-06 15:26:32 +0100

    Added delete player testing

--- server/spec/support/Server.spec.ts
+++ server/spec/support/Server.spec.ts
@@ -30,6 +30,28 @@ it("creates a player", async function () {
   expect(result).toEqual("OK");
 }, 5000);
 
+it("deletes a player", async function () {
+  await dao.createGame(new Game("game123"));
+  await dao.createPlayer(new Player("player123", "Diego"));
+  const joinGameResult = await dao.joinGame("game123", "player123");
+  const game = await dao.getGameById("game123");
+  const player = await dao.getPlayerById("player123");
+
+  if (player === null) {
+    throw "player does not exist";
+  }
+
+  expect(joinGameResult).toEqual("OK");
+  expect(game._data.players).toContain("player123");
+  expect(player._data.gameId).toEqual("game123");
+
+  await dao.deletePlayer("player123");
+  const playerNull = await dao.getPlayerById("player123");
+  const gameAfterPlayerDeleted = await dao.getGameById("game123");
+  expect(gameAfterPlayerDeleted._data.players).not.toContain("player123");
+  expect(playerNull).toEqual(null);
+}, 5000);
+
 it("gets a game", async function () {
   await dao.createGame(new Game("game123"));
   const savedGame = await dao.getGameById("game123");
@@ -50,6 +72,9 @@ it("joins a game", async function () {
   const joinGameResult = await dao.joinGame("game123", "player123");
   const game = await dao.getGameById("game123");
   const player = await dao.getPlayerById("player123");
+  if (player === null) {
+    throw "player does not exist";
+  }
   expect(joinGameResult).toEqual("OK");
   expect(game._data.players).toContain("player123");
   expect(player._data.gameId).toEqual("game123");
@@ -64,6 +89,9 @@ it("get all players of a game", async function () {
   const allPlayers = await dao.getAllPlayersByGameId("game123");
   for (const player of allPlayers) {
     const savedPlayer = await dao.getPlayerById(player._id);
+    if (savedPlayer === null) {
+      throw "player does not exist";
+    }
     expect(player).toEqual(savedPlayer);
   }
 }, 5000);
@@ -72,6 +100,9 @@ it("set a player as ready", async function () {
   await dao.createPlayer(new Player("player123", "Diego"));
   await dao.setPlayerReady("player123", "game123", true);
   const player = await dao.getPlayerById("player123");
+  if (player === null) {
+    throw "player does not exist";
+  }
   console.log(`Player with ready as true == ${player._data.ready}`);
   expect(player._data.ready).toBe(true);
 });
@@ -84,7 +115,7 @@ it("sets a game as in progress", async function () {
   expect(game._data.started).toBe(true);
 });
 
-it("check if all players of a game are ready", async function () {
+it("checks if all players of a game are ready", async function () {
   await dao.createGame(new Game("game123"));
 
   await dao.createPlayer(new Player("player123", "Diego"));
