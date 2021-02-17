2ee666b71cf9bf0a5656c448af908c5983d1421d
diegovargasg <diegodevelopero@gmail.com>
2021-02-17 21:32:18 +0100

    cleaned waiting component

--- client/src/app/modules/waiting/waiting.component.ts
+++ client/src/app/modules/waiting/waiting.component.ts
@@ -1,9 +1,5 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 import { Player } from 'src/app/core/models/player.model';
-import { GameService } from '../../core/services/game.service';
-import { PlayerService } from '../../core/services/player.service';
 
 @Component({
   selector: 'app-waiting',
@@ -22,20 +18,7 @@ export class WaitingComponent implements OnInit {
     this._allPlayers = allPlayers;
   }
 
-  constructor(
-    private router: Router,
-    private gameService: GameService,
-    private playerService: PlayerService
-  ) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    //this.gameService.getAllPlayersByGameId(this.gameService.id);
-  }
-
-  /*ngOnChanges(changes: SimpleChanges) {
-    console.log(changes);
-    //this.doSomething(changes.categoryId.currentValue);
-    // You can also use categoryId.previousValue and
-    // categoryId.firstChange for comparing old and new values
-  }*/
+  ngOnInit(): void {}
 }
