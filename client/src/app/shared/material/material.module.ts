5a2d6a735db40239180573341069858fa5fb5ff5
diegovargasg <diegodevelopero@gmail.com>
2021-02-18 22:03:49 +0100

    added basic styles players

--- client/src/app/shared/material/material.module.ts
+++ client/src/app/shared/material/material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule, MatInputModule, MatFormFieldModule],
-  exports: [MatButtonModule, MatInputModule, MatFormFieldModule],
+  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatListModule],
+  exports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatListModule],
 })
 export class MaterialModule {}
