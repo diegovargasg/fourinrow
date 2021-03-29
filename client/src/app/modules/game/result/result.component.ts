f185d0e9bda2015c8ab7822404923cfd6eb74d47
diegovargasg <diegodevelopero@gmail.com>
2021-03-29 18:26:22 +0200

    modified results view

--- client/src/app/modules/game/result/result.component.ts
+++ client/src/app/modules/game/result/result.component.ts
@@ -3,6 +3,14 @@ import { GameService } from 'src/app/core/services/game.service';
 import { Player } from '../../../core/models/player.model';
 import { Router } from '@angular/router';
 
+interface finalResults {
+  [key: string]: {
+    wrong: number;
+    correct: number;
+    winner: boolean;
+  };
+}
+
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
@@ -10,12 +18,24 @@ import { Router } from '@angular/router';
 })
 export class ResultComponent implements OnInit {
   private _allPlayers: Player[] = [];
+  public finalResults: finalResults = {};
 
   @Input()
   get allPlayers(): Player[] {
     return this._allPlayers;
   }
   set allPlayers(allPlayers: Player[]) {
+    allPlayers.forEach((player) => {
+      const correct = player._data.results.filter((result) => {
+        return result === true;
+      }).length;
+      const wrong = player._data.results.length - correct;
+      this.finalResults[player._data.name] = {
+        wrong: wrong,
+        correct: correct,
+        winner: false,
+      };
+    });
     this._allPlayers = allPlayers;
   }
 
